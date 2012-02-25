1c04f3b3081522a823a564dfe3782fc24b07e4e5
Artem Dudarev <dudarev@gmail.com>
2012-02-25 18:09:56 +0200

    issue:1 pages spreadsheet is saved to _pages directory, new option not to do it in get

--- goodoc.py
+++ goodoc.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import os
 import sys
 
 from settings import *
@@ -6,7 +7,29 @@ from settings import *
 
 def get():
     "downloads spreadsheet with list of pages and each HTML for each page"
-    print "downloads spreadsheet with list of pages and each HTML for each page"
+
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-n", "--no-pages", action="store_true", dest="no pages")
+    (opts, args) = parser.parse_args(sys.argv[2:])
+
+    if not getattr(opts,"no pages"):
+        # download pages spreadsheet if -n option is NOT specified
+        print "downloading link pages spreadsheet..."
+
+        import urllib2
+        response = urllib2.urlopen(PAGES_LINK)
+        html = response.read()
+
+        if not os.path.exists(RAW_PAGES_DIR):
+            print "Create directory for pages."
+            os.makedirs(RAW_PAGES_DIR)
+        
+        f = open(os.path.join(RAW_PAGES_DIR, 'pages.html'), 'w')
+        f.write(html)
+    
+    else:
+        print "pages spreadsheet is NOT downloaded"
 
 def help():
     "prints help"
@@ -16,6 +39,7 @@ def help():
     print "commands:"
     print ""
     print "  get    downloads spreadsheet with list of pages and each HTML for each page"
+    print "     -n  do not download pages spreadseet"
     print ""
 
 def main():
--- settings.py
+++ settings.py
@@ -1,1 +1,5 @@
 PAGES_LINK = "https://docs.google.com/spreadsheet/pub?key=0Ar7fZWxISpOKdHU1WHlKb09EbFJMV09jX3NkZUlsWVE&output=html"
+
+# You probably do not want to change these settings
+
+RAW_PAGES_DIR = "_pages"
